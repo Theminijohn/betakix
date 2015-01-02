228bdcbe17bc4320a310f56ee490f588b5437b78
The Mini John <admin@phoenix-prime.gr>
2015-01-02 20:11:09 +0100

    Chart Graph Baisc

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,19 @@
+// Major Components of Graph Class
+
+var Graph = function(selector, data, kind) {
+  this.selector = selector;
+  this.data = data;
+  this.kind = kind;
+};
+
+
+Graph.prototype.getData = function() {
+}
+
+Graph.prototype.render = function(){
+}
+
+
+Graph.instances = [];
+
+
