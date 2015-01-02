7cc3dcdf6418e853df67dc5b8880686a9e73bbd0
The Mini John <admin@phoenix-prime.gr>
2015-01-02 20:48:32 +0100

    Poll Answers - Gathering Data

--- app/lib/poll_serializer.rb
+++ app/lib/poll_serializer.rb
@@ -8,6 +8,7 @@
 
 class PollSerializer
 
+  ## Polls Answered by Month
   def self.count_per_month(poll)
     replies = poll.replies.group_by { |reply| reply.created_at.beginning_of_month }
     data = replies.map { |key, values| values.length } 
@@ -19,7 +20,26 @@ class PollSerializer
         series: replies.keys.map { |date| date.strftime("%b %Y") } # []
       },
       y_axis: {
-        legend: 'No. polls',
+        legend: 'No# polls',
+        scale: [0, data.max + 1]
+      }
+    }
+  end
+
+  ## Answers Per Question
+  def self.answers_per_question(question)
+    answers_per_question= question.answers.group_by(&:possible_answer) 
+    data = answers_per_question.map { |possible_answer, answers| answers.length }
+    series = answers_per_question.map { |possible_answer, answers| possible_answer.title }
+    {
+      data: data, # []
+      title: 'Answers per Question',
+      x_axis: {
+        legend: 'Polls per month',
+        series: series
+      },
+      y_axis: {
+        legend: 'No# polls',
         scale: [0, data.max + 1]
       }
     }
